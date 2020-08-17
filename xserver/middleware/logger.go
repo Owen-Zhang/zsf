0c37af42310d95a4c2c4bc72384d96891f64bde2
Owen-Zhang <1034992601@qq.com>
2020-08-17 10:26:01 +0800

    记录日志修改

--- xserver/middleware/logger.go
+++ xserver/middleware/logger.go
@@ -12,6 +12,6 @@ func Log() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		start := time.Now()
 		c.Next()
-		logger.FrameLog.Infof("[method:%s];[path:%s];[status:%d];[time:%d (ms)]", c.Request.Method, c.Request.URL.Path, c.Writer.Status(), time.Since(start)/time.Millisecond)
+		logger.FrameLog.Infof("[method:%s];[path:%s];[status:%d];[time:%.1f(ms)]", c.Request.Method, c.Request.URL.Path, c.Writer.Status(), float32(time.Since(start))/float32(time.Millisecond))
 	}
 }
