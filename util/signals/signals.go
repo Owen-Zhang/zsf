0376990dc18d9d5eb746d73f8cf01f0b5c14e216
Owen-Zhang <1034992601@qq.com>
2020-11-03 20:52:06 +0800

    wait signal

--- util/signals/signals.go
+++ util/signals/signals.go
@@ -1,7 +1,6 @@
 package signals
 
 import (
-	"fmt"
 	"os"
 	"os/signal"
 )
@@ -11,12 +10,11 @@ func Shutdown(stop func()) {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, shutdownSignals...)
 
-	select {
-	case <-sig:
-		fmt.Printf("捕获到结束程序信号, 正在线束中... pid=%d\n", os.Getpid())
-	}
-	if stop != nil {
-		stop()
-	}
-	os.Exit(0)
+	go func() {
+		<-sig
+		if stop != nil {
+			stop()
+		}
+		os.Exit(0)
+	}()
 }
